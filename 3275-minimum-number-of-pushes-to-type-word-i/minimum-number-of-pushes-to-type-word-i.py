4f98b9f677763bd865cc8f20263e2ee47bf8fa3f
Gunda Saahith <saahithgunda@gmail.com>
2026-07-30 10:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (16.35%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -2,9 +2,5 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
         n = len(word)
-        ans = 0
-        if n < 9:
-            return n
-        for i in range(1,(n//8)+1):
-            ans += 8*i
-        return ans + (n % 8)*((n//8)+1)
+        blocks = n//8
+        return (blocks *(blocks+1)*4)+(blocks+1)*(n % 8)
